77b4cbaaa20507de765686a022ba66fea5fa8a06
wjdals0509 <wjdals050932@gmail.com>
2024-03-18 00:27:03 +0900

    todo 삭제 기능 추가

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
   // target.value는 input field의 값을 나타냄
   // 이 값을 setToDo 함수에 전달
   const onChange = (event) => setToDo(event.target.value);
+  // toDo 추가하기
   // form은 submit 이벤트를 기본적으로 갖고있기 때문에
   // event.preventDefault()로 기본동작인 새로고침을 막음
   const onSubmit = (event) => {
@@ -29,6 +30,11 @@ function App() {
     //    setToDo 함수를 사용해서 수정해야함
     setToDo("");
   };
+  // toDo 삭제하기
+  const deleteBtn = (index) => {
+    setToDos(toDos.filter((item, todoIndex) => index !== todoIndex));
+  };
+
   return (
     <div>
       {/* toDo의 갯수 출력 */}
@@ -51,7 +57,10 @@ function App() {
         {toDos.map((item, index) => (
           // 동일한 component의 리스트를 render할 때는 key라는 prop을 넣어야함
           // key는 고유한 값이어야 하기 때문에 index를 넣어줌
-          <li key={index}>{item}</li>
+          <li key={index}>
+            {item}
+            <button onClick={() => deleteBtn(index)}>❎</button>
+          </li>
         ))}
       </ul>
     </div>
