4fc4a1fbd4527703f8ce9225b1760f93a890dbba
wjdals0509 <wjdals050932@gmail.com>
2024-03-17 23:13:53 +0900

    value(=toDo)가 State에 등장

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
+import { useState } from "react";
+
 function App() {
+  // input field control
+  // todo : input의 value를 받아오는 변수
+  // setTodo : value를 수정하는 함수
+  const [toDo, setToDo] = useState(""); // default는 공백 String값
+  const onChange = (event) => setToDo(event.target.value);
+  console.log(toDo);
   return (
     <div>
-      <h1>Test</h1>
+      <form>
+        {/* to do 입력*/}
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do..."
+        />
+      </form>
     </div>
   );
 }
